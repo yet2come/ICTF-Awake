6d96008562cb71abfb5e1bd62ddb456cbd79b373
Takuji Akiyama <38804910+yet2come@users.noreply.github.com>
2019-10-19 03:25:28 +0900

    Update _siteConfig.js

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -61,11 +61,11 @@ export default {
     btnText: 'Subscribe',
     // Can be the form action on a mail chimp form, a hubspot form,
     // or any other url you want to post the form data to
-    mailchimp: {
-      on: true,
-      formAction:
-        'https://danielkelly.us3.list-manage.com/subscribe/post?u=d3c2e762b66a2353d0bc29bf3&amp;id=2abeaa06e0'
-    },
+//    mailchimp: {
+//      on: true,
+//      formAction:
+//        'https://danielkelly.us3.list-manage.com/subscribe/post?u=d3c2e762b66a2353d0bc29bf3&amp;id=2abeaa06e0'
+//    },
     custom: {
       on: false,
       formAction: ''
