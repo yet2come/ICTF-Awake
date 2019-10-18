f615cd5b13bf83ae98134b0baa3cbfa3fb64dc0e
Takuji Akiyama <38804910+yet2come@users.noreply.github.com>
2019-10-19 03:35:53 +0900

    Update about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -2,7 +2,7 @@
   <div id="about-page" class="page-wrapper about-page content-page">
     <site-hero
       title="About Us"
-      subtitle="Example About Page"
+      subtitle="All about ICTF"
       image="/uploads/about-hero.jpg"
     ></site-hero>
     <main-section theme="sidebar-right">
