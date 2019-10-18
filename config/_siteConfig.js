ac2c6f137a304aa2ecca2cb8fde0ddee3abb68a2
Takuji Akiyama <38804910+yet2come@users.noreply.github.com>
2019-10-19 04:04:30 +0900

    Update _siteConfig.js

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -12,7 +12,7 @@ export default {
   siteName: 'ICTF',
   tagline: 'Internet Community Talking about Future',
   featureImage: '/uploads/home-hero.jpg',
-  logo: 'logo-component', // 'logo-component', // or '/logo.svg' for regular image
+  logo: 'logo.svg', // 'logo-component', // or '/logo.svg' for regular image
   googleAnalytics: {
     on: true,
     id: process.env.GOOGLE_ANALYTICS_ID
